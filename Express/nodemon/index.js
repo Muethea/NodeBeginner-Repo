41ad34d13981ced8421d6df655e1fcc851509336
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2023-08-09 13:59:05 +0200

    nodemon update

---
+++ Express/nodemon/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+const path = require('path')
+
+const pathBase = path.join(__dirname, 'template')
+
+app.get('/', (req, res) => {
+  res.sendFile(`${pathBase}/index.html`)
+})
+
+app.listen(port, () => {
+  console.log(`Esta rodando na porta ${port}`)
+})
