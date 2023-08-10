a83285aefc5d5e65401e54e150c889caa18a0971
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2023-08-10 13:19:08 +0200

    Post data

--- Express/postDados/index.js
+++ Express/postDados/index.js
@@ -2,25 +2,40 @@ const express = require('express')
 
 const app = express()
 
-const port = 3001
+const port = 3000
 
 const path = require('path')
 
-const basePath = path.join(__dirname, 'template')
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
 
-app.get('/users/add', (req, res) => {
-  res.sendFile(`${basePath}/userForm.html`)
-})
+app.use(express.json())
+
+const pathBase = path.join(__dirname, 'template')
 
 app.post('/users/save', (req, res) => {
-
   console.log(req.body)
+
+  const nome = req.body.name
+
+  const age = req.body.age
+
+  console.log(`O nome é ${nome} e a idade é ${age}`)
+
+  res.sendFile(`${pathBase}/userForm.html`)
+})
+
+app.get('/users/add', (req, res) => {
+  res.sendFile(`${pathBase}/userForm.html`)
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(`${basePath}/index.html`)
+  res.sendFile(`${pathBase}/index.html`)
 })
 
 app.listen(port, () => {
-  console.log(`Executando na porta ${port}`)
+  console.log(`Rodando na porta ${port}`)
 })
